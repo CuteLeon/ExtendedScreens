2d91575d26040218d2bb163d2272dd52e6025796
Leon <xiaoyankeji@hotmail.com>
2016-08-01 19:21:32 +0800

    第一次提交

---
+++ 扩展屏幕/Form1.designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripMenuItem3 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem4 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 40
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1, Me.ToolStripMenuItem2, Me.ToolStripSeparator1, Me.ToolStripMenuItem3, Me.ToolStripMenuItem4})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.ShowImageMargin = False
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(100, 98)
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(99, 22)
+        Me.ToolStripMenuItem1.Text = "显示双屏"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(99, 22)
+        Me.ToolStripMenuItem2.Text = "仅扩展屏"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(96, 6)
+        '
+        'ToolStripMenuItem3
+        '
+        Me.ToolStripMenuItem3.Name = "ToolStripMenuItem3"
+        Me.ToolStripMenuItem3.Size = New System.Drawing.Size(99, 22)
+        Me.ToolStripMenuItem3.Text = "正常尺寸"
+        '
+        'ToolStripMenuItem4
+        '
+        Me.ToolStripMenuItem4.Name = "ToolStripMenuItem4"
+        Me.ToolStripMenuItem4.Size = New System.Drawing.Size(99, 22)
+        Me.ToolStripMenuItem4.Text = "全屏模式"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.ClientSize = New System.Drawing.Size(360, 320)
+        Me.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.TopMost = True
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ToolStripMenuItem3 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem4 As ToolStripMenuItem
+End Class
---
+++ 扩展屏幕/Form1.vb
@@ -0,0 +1,103 @@
+﻿Public Class Form1
+    '仅适用于双屏扩展显示
+    '按 [Win + P] 打开“投影面板”，选择“扩展”
+
+    Private Declare Function ReleaseCapture Lib "user32" () As Integer
+    Private Declare Function SendMessageA Lib "user32" (ByVal hwnd As Integer, ByVal wMsg As Integer, ByVal wParam As Integer, lParam As VariantType) As Integer
+    Private Declare Function GetWindowLong Lib "user32.dll" Alias "GetWindowLongA" (ByVal hwnd As Int32, ByVal nIndex As Int32) As Int32
+    Private Declare Function SetWindowLong Lib "user32.dll" Alias "SetWindowLongA" (ByVal hwnd As Int32, ByVal nIndex As Int32, ByVal dwNewLong As Int32) As Int32
+    Private Const GWL_STYLE As Int32 = -16
+    Private Const WS_THICKFRAME As Int32 = &H40000
+
+    Dim b As Bitmap = New Bitmap(1366, 768)
+    Dim g As Graphics
+
+    Dim Dis As Integer = 0
+    Dim AspectRatio As Double
+    Dim DoubleScreen As Boolean = False
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If Screen.AllScreens.Count = 1 Then Exit Sub
+
+        Me.TopMost = True
+        b.Dispose()
+        If DoubleScreen Then
+            '双屏截图
+            b = New Bitmap(Screen.AllScreens.Last.Bounds.Width + Screen.AllScreens.First.Bounds.Width, Math.Max(+Screen.AllScreens.Last.Bounds.Height, Screen.AllScreens.First.Bounds.Height))
+        Else
+            '仅扩展屏
+            b = New Bitmap(Screen.AllScreens.Last.Bounds.Width, Screen.AllScreens.Last.Bounds.Height)
+        End If
+
+        g = Graphics.FromImage(b)
+
+
+        If DoubleScreen Then
+            '双屏截图
+            g.CopyFromScreen(New Point(0, 0), New Point(0, 0), New Size(Screen.AllScreens.Last.Bounds.Width + Screen.AllScreens.First.Bounds.Width, Math.Max(+Screen.AllScreens.Last.Bounds.Height, Screen.AllScreens.First.Bounds.Height)))
+        Else
+            '仅扩展屏
+            g.CopyFromScreen(New Point(Screen.AllScreens.First.Bounds.Width, 0), New Point(0, 0), Screen.AllScreens.Last.Bounds.Size)
+        End If
+
+        '绘制鼠标位置
+        If DoubleScreen Then
+            '双屏截图
+            g.DrawEllipse(Pens.Red, New RectangleF(MousePosition.X - 25, MousePosition.Y - 25, 50, 50))
+            g.FillEllipse(Brushes.Red, New RectangleF(MousePosition.X - 5, MousePosition.Y - 5, 10, 10))
+        Else
+            '仅扩展屏
+            If Screen.FromPoint(MousePosition).Equals(Screen.AllScreens.Last) Then
+                g.DrawEllipse(Pens.Red, New RectangleF(MousePosition.X - Screen.AllScreens.First.Bounds.Width - 25, MousePosition.Y - 25, 50, 50))
+                g.FillEllipse(Brushes.Red, New RectangleF(MousePosition.X - Screen.AllScreens.First.Bounds.Width - 5, MousePosition.Y - 5, 10, 10))
+            End If
+        End If
+
+        Me.BackgroundImage = b
+
+        g.Dispose()
+        GC.Collect()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim MyWindowStyle As Int32 = GetWindowLong(Me.Handle, GWL_STYLE) Or WS_THICKFRAME
+        SetWindowLong(Me.Handle, GWL_STYLE, MyWindowStyle)
+
+        AspectRatio = Screen.AllScreens.Last.Bounds.Width / Screen.AllScreens.Last.Bounds.Height
+    End Sub
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+        ReleaseCapture()
+        SendMessageA(Me.Handle, &HA1, 2, 0&)
+    End Sub
+
+    Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem1.Click
+        DoubleScreen = True
+
+        AspectRatio = (Screen.AllScreens.First.Bounds.Width + Screen.AllScreens.Last.Bounds.Width) / Math.Max(+Screen.AllScreens.Last.Bounds.Height, Screen.AllScreens.First.Bounds.Height)
+        Me.Height = Me.Width / AspectRatio + Dis
+    End Sub
+
+    Private Sub ToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem2.Click
+        DoubleScreen = False
+
+        AspectRatio = Screen.AllScreens.Last.Bounds.Width / Screen.AllScreens.Last.Bounds.Height
+        Me.Width = (Me.Height - Dis) * AspectRatio
+    End Sub
+
+    Private Sub ToolStripMenuItem3_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem3.Click
+        Me.WindowState = FormWindowState.Normal
+        AspectRatio = Screen.AllScreens.Last.Bounds.Width / Screen.AllScreens.Last.Bounds.Height
+        Me.Width = (Me.Height - Dis) * AspectRatio
+    End Sub
+
+    Private Sub ToolStripMenuItem4_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem4.Click
+        Me.WindowState = FormWindowState.Maximized
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        If Me.WindowState = FormWindowState.Normal Then
+            Me.Width = (Me.Height - Dis) * AspectRatio
+        End If
+    End Sub
+End Class
